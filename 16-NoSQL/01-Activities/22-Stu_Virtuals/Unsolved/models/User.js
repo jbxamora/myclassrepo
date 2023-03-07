0d1912d46fe4a348a4c6cf3650b41bb0daa60753
jbxamora <jbxamora@icloud.com>
2023-03-06 19:10:39 -0800

    updated activity 22 in NOSQL

--- 16-NoSQL/01-Activities/22-Stu_Virtuals/Unsolved/models/User.js
+++ 16-NoSQL/01-Activities/22-Stu_Virtuals/Unsolved/models/User.js
@@ -9,16 +9,26 @@ const userSchema = new Schema(
   {
     toJSON: {
       // TODO: Mongoose will not include virtuals by default, so add a `virtuals` property and set it's value to true
+      virtuals: true,
     },
     id: false,
   }
 );
 
 // TODO: Create a virtual property `fullName` on the userSchema
-
 // TODO: Create a getter for the virtual that returns the full name of the user (first + last)
-
 // TODO: Create a setter for the virtual that sets the value of the first and last name, given just the `fullName`
+
+userSchema.virtual('fullName', {
+  get() {
+    return `${this.first} ${this.last}`;
+  },
+  set(value) {
+    const [first, last] = value.split(' ');
+    this.set({ first, last });
+  },
+});
+
 
 // Initialize our User model
 const User = model('user', userSchema);
